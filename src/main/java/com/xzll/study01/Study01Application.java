0f778454f5882cc92d122b79fb9d39977989b858
黄壮壮 <h163361631@163.com>
2021-05-22 15:23:09 +0800

    项目本地仓库上传

---
+++ src/main/java/com/xzll/study01/Study01Application.java
@@ -0,0 +1,13 @@
+package com.xzll.study01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzll/study01/Study01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzll.study01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
